dfbe7e535d82e56335e06bee985cb68c31a14faa
ngok0495-beep <ngok0495@gmail.com>
2026-09-10 16:26:39 +0700

    Add: longest-consecutive-sequence - submission-16

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-16.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        let longest = 0
+        let numSet = new Set(nums)
+
+        for(let n of numSet){
+            if(!numSet.has(n - 1)){
+                let length = 1
+            
+            while(numSet.has(n + length)){
+                length++
+            }
+        
+            longest = Math.max(length, longest)
+            }
+        }
+        return longest
+    }
+}
